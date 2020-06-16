d2588e15408c4d2860d3250e5bbb04e08014eefa
Wersario <60400132+Wersario@users.noreply.github.com>
2020-06-16 19:34:56 +0300

    little changes

--- manage.py
+++ manage.py
@@ -7,7 +7,6 @@ for i in range(1, 11):
     url = f'http://tululu.org/txt.php?id={i}/'
     ans = requests.get(url, allow_redirects=False)
     if((ans.status_code // 100) == 2):
-        print("OK")
         with open(f"/Users/Юрис/Desktop/library/books/id{i}.txt", "w") as my_file:
             my_file.write(ans.text)
 
