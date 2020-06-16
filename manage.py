88d468bbc3faabdf62d136129504b974fa852264
Wersario <60400132+Wersario@users.noreply.github.com>
2020-06-16 19:34:34 +0300

    download books from tululu.org

--- manage.py
+++ manage.py
@@ -1,8 +1,13 @@
 import requests
+import os
 
 
-url = 'http://tululu.org/txt.php?id=32168'
-answer = requests.get(url)
-answer.raise_for_status()
+os.makedirs("/Users/Юрис/Desktop/library/books", exist_ok=True)
+for i in range(1, 11):
+    url = f'http://tululu.org/txt.php?id={i}/'
+    ans = requests.get(url, allow_redirects=False)
+    if((ans.status_code // 100) == 2):
+        print("OK")
+        with open(f"/Users/Юрис/Desktop/library/books/id{i}.txt", "w") as my_file:
+            my_file.write(ans.text)
 
-print(answer.text)
