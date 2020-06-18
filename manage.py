46543cfcfd89e40a8acfbba1db4d5f41276c0626
Wersario <60400132+Wersario@users.noreply.github.com>
2020-06-18 19:54:40 +0300

    download books with titles

--- manage.py
+++ manage.py
@@ -1,11 +1,29 @@
 import requests
+import os
 from bs4 import BeautifulSoup
+from pathvalidate import sanitize_filename
 
-url = 'http://tululu.org/b1/'
-response = requests.get(url)
-response.raise_for_status()
-soup = BeautifulSoup(response.text, 'lxml')
-main_inf = soup.find('div', id='content').find('h1').text
-inf_list = main_inf.split(' :: ')
-print("Заголовок:", inf_list[0].strip())
-print("Автор:", inf_list[1].strip())
+
+def download_text(url, filename, folder):
+    base = '/Users/Юрис/Desktop/library/'
+    fin_filename = os.path.join(base, folder, sanitize_filename(filename))
+    ans = requests.get(url)
+    ans.raise_for_status()
+    with open(fin_filename, "w", encoding="UTF-8") as my_file:
+        my_file.write(ans.text)
+
+
+os.makedirs("/Users/Юрис/Desktop/library/books", exist_ok=True)
+for i in range(1, 11):
+    text_url = f'http://tululu.org/txt.php?id={i}'
+    html_url = f'http://tululu.org/b{i}/'
+    main_ans = requests.get(text_url, allow_redirects=False)
+    main_ans.raise_for_status()
+    if main_ans.status_code // 100 == 2:
+        response = requests.get(html_url)
+        soup = BeautifulSoup(response.text, 'lxml')
+        title_and_author = soup.find('div', id='content').find('h1').text
+        filename = title_and_author.split(' :: ')[0].strip()
+        fin_filename = filename + ".txt"
+        download_text(text_url, fin_filename, 'books/')
+
